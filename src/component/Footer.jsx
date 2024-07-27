e3d2a6d30fc60e1f1c46db684ee5765d9d09754f
Ashishs799 <np03cs4s220007@heraldcollege.edu.np>
2024-07-27 22:34:45 +0545

    Added Footer

--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -1,9 +1,44 @@
 import React from "react";
 import "../styles/Footer.css";
+import { AiFillInstagram } from "react-icons/ai";
+import { FaFacebook } from "react-icons/fa";
+import { FaTwitter } from "react-icons/fa";
+
 export const Footer = () => {
   return (
     <footer>
-      <h1>Footer Loading.......</h1>
+      <h1>Sneakerzz</h1>
+      <div className="flex footer_wrapper">
+        <div className="footer_links">
+          <ul className="flex_column links">
+            <li>Men</li>
+            <li>Women</li>
+            <li>Sneakers</li>
+            <li>About us</li>
+            <li>Contact us</li>
+          </ul>
+        </div>
+        <div className="footer_links column">
+          <ul className="flex_column links">
+            <li>sneakerzz@gmail.com</li>
+            <li>Koteshwar-32, Kathmandu</li>
+            <li>Phone: +977 9805834618</li>
+          </ul>
+          <div>
+            <AiFillInstagram />
+            <FaFacebook />
+            <FaTwitter />
+          </div>
+        </div>
+        <div className="footer_links">
+          <span>Receive all our news and updates</span>
+          <input type="text" placeholder="Enter your email here*" />
+          <button className="button subscribe_btn">Subscribe now</button>
+        </div>
+      </div>
+      <div className="copyright flex">
+        <span>copyright &copy;2024 Sneakerzz | Powered by Sneakerzz</span>
+      </div>
     </footer>
   );
 };
